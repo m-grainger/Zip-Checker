eadd282d69c72f20b190b95ea2133f1a3b0099d3
Matt Grainger <grainger.pdx@gmail.com>
2018-11-12 13:39:09 -0800

    Update to main.py
    Added lots of stuff.  There are now more redundancy checks!  
    Files in the zips matching ".xml" are counted and reported, and final_counts.txt was cleaned up, with more meaningful information added.

--- zip-checker.py
+++ zip-checker.py
@@ -5,7 +5,7 @@ import time
 
 def user_inputs():
 # user input variables
-	user_count = input("Please enter the number of files that you are expecting to be scanned: ") 
+	user_count = input("Please enter the number of zip files to be scanned:  ") 
 	infolder = input("Please set the drive path (e.g. M:): " )
 	password = input("Please enter the password: ")
 	password = f'"{password}"'
@@ -13,11 +13,11 @@ def user_inputs():
 
 choices = user_inputs() # <---- Code starts running here!
 
-
 # user verifies everything that is written above is correct 
 print(f"\n\nNumber of expected files to be scanned: {choices[0]}")
 print(f"Drive that is going to be scanned: {choices[1]}")
-print(f"Expected password for the files on the drive: {choices[2][1:-1]}\n")
+pass_choice = choices[2]
+print(f"Expected password for the files on the drive: {pass_choice[1:-1]}\n")
 
 # making sure that the above information is correct
 g2g = input("Does this look correct?  [Y / N]: \n")
@@ -36,11 +36,11 @@ for path, subdirs, files in os.walk(choices[1]):
     for name in files:
     	if zip_check in name:
         	hd_file_list.append(os.path.join(path, name)) # only inserts into list if ".zip" is in the file name
-
-     
+    
 # creates a text file that replicates ZipTest.bat 
 with open("ZipTest.txt", "a") as f:
-	f.write(f"SET logfolder=%CD%\\logs\\") 
+	f.write(f"SET logfolder=%CD%\\logs\\")
+	f.write(f"\nSET countfolder=%CD%\\counts\\")  
 	f.write(f"\nSET taillines=5")
 	f.write(f"\n") # just adding a blank line here
 	f.write(f"\nSET rundir=%CD%")
@@ -52,6 +52,8 @@ with open("ZipTest.txt", "a") as f:
 	f.write(f'\n{"    "}echo -- End %%~ng -- >> %logfolder%\\Results.txt')
 	f.write(f'\n{"    "}echo. >> %logfolder%\\Results.txt')
 	f.write(f'\n{"    "})')
+	f.write(f"\nmd %countfolder%") 
+	f.write(f'\nfor /R {choices[1]} %%g in (*.zip) DO %rundir%\\7za.exe l -r -p{choices[2]} "%%~fg" 1> "%countfolder%\\%%~nxg.log.txt" 2>&1')
 
 os.rename(f"{os.getcwd()}\\ZipTest.txt","ZipTest.bat") # renames the ZipTest.txt file to ZipTest.bat
 
@@ -91,17 +93,80 @@ for x in text_list:
 check = f"{os.getcwd()}\\logs\\Results.txt"
 err_final = [x for x in error_list if check not in x]
 
-for x in set(err_final): print(f"Error found in: ...{x[-30:]}")
+for x in set(err_final): print(f"Error found in: ...{x[-50:]}")
+
+# logic that compares a list of zip files on the drive to zip log files
+def comparison_check():
+	zip_check = ".zip"
+	hd_file_list = []
+	for path, subdirs, files in os.walk(choices[1]):
+	    for name in files:
+	    	if zip_check in name:
+	        	hd_file_list.append(os.path.join(name)) 
+
+	text_check = ".txt"
+	text_file_list = []
+	text_file_list_full = []
+	for path, subdirs, files in os.walk(f"{os.getcwd()}\\logs\\"):
+	    for name in files:
+	    	if text_check in name and "Results.txt" not in name:
+	        	text_file_list.append(os.path.join(name[:-8]))
+	        	text_file_list_full.append(os.path.join(name,path)) 
+
+	if hd_file_list.sort() == text_file_list.sort():
+		comp_bool = True
+		return comp_bool
+	else:
+		comp_bool = False
+		return comp_bool
+
+comparison_bool = comparison_check()
+
+# generates list of zipped files on the chosen drive path.  Outputs to "Manifest.txt"
+def manifest_gen():
+	zip_check = ".zip"
+	zip_file_list_path = []
+	for path, subdirs, files in os.walk(choices[1]):
+	    for name in files:
+	    	if zip_check in name:
+	        	zip_file_list_path.append(os.path.join(path,name)) 
+
+	return zip_file_list_path
+zflp = manifest_gen()
+
+# grabs totals of files contained in a zip file.
+counts_list = []
+for path, subdirs, files in os.walk(os.getcwd()+"\\counts"):
+    for name in files:
+        s = os.path.join(path, name)
+        counts_list.append(os.path.join(path, name))
+
+print("\ngetting zip totals...")
+time.sleep(2.5)
+counts_totals = 0
+for x in counts_list:
+	with open(x, 'r', encoding= "utf-8", errors= "ignore") as searchfile:
+		for line in searchfile:
+			if '.xml' in line:			
+				counts_totals += 1
 
 # creates a  text document with final counts + list of errored zip files
 with open("final_counts.txt","a") as g:                                                                      
-	g.write(f"Total number of files that you entered: {choices[0]}")                                         
-	g.write(f"\nTotal Count of files that successfully scanned with no errors: {results_count}")               
-	g.write("\n")
-	g.write(f"Total Count of zip files located in the drive {choices[1]} and it's subdirectories: {len(hd_file_list)}\n\n") # <-- actual file count 
-	for x in set(err_final): g.write(f"Error detected: {x}\n")
+	g.write(f"\n({choices[0]}) | Expected Number of zips")
+	g.write(f"\n({len(hd_file_list)}) | Count of zip files on {choices[1]} and it's subdirectories") # <-- actual file count                                         
+	g.write(f"\n({results_count}) | Total Count of Zips that successfully scanned with no password errors")               
+	g.write(f"\n({counts_totals}) | XML count from within zips\n")
+	g.write(f"\n({len(set(err_final))}) | Number of zips with password errors\n") 
+	for x in set(err_final): g.write(f"\nError detected: {x}")
+	g.write(f"\n\nAll files successfully copied and scanned = {comparison_bool}")
 
+with open("Manifest.txt","a") as h:
+	for x in zflp: h.write(f"\n{x}")    #<-- fix this!	
 
-print("\n\nScanning and Logging Complete.\nA file named 'final_counts.txt' has been added to your current working directory")
+print("\nA file named 'final_counts.txt' has been added to your current working directory")
+print("\nA file named 'Manifest.txt' has added to your current working directory")
+print("\nScan complete.")
 time.sleep(5)
 sys.exit()
+
+# 	#add logic that shows if everything matches or not, and writes it to the text file
